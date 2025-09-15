c44b00bb3ef807df09b5fd3561e53157253d6126
cyrilmichino <cyrilmichino@gmail.com>
2025-09-15 06:48:12 +0300

    Base Components and Homepage

---
+++ src/consts.ts
@@ -0,0 +1,4 @@
+// All global data exists in this file
+
+export const SITE_TITLE = 'Open Deploy';
+export const SITE_DESCRIPTION = 'Collection of open-source deployment guides for various open-source applications that are alternatives to common SaaS apps. Complied by Cyril Michino.';
